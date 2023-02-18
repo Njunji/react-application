6460f89bca384611508517d251d7ddea2546c3ad
Njunji Muchai <samwelnjunji@gmail.com>
2023-02-19 01:18:45 +0300

    Added hook

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import {useState} from 'react';
 import './App.css';
 
 const App = () => {
+  const[counter, setCounter] = useState(0);
   return (
     <div className="App">
-       <h1>Hello React!</h1>
+      <button onClick={() => setCounter((prevCount)=> prevCount - 1)}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((nextCount)=> nextCount + 1)}>+</button>
     </div>
   );
 }
